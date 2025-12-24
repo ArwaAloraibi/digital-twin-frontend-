1e17bc9a11ed7d24fba90f44d1af100c274b0536
Arwa Aloraibi <arwa7oraibi@gmail.com>
2025-12-24 13:19:15 +0300

    edit the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // src/App.jsx
 
-import { Routes, Route } from 'react-router'; // Import React Router
-
+import { Routes, Route, useParams } from 'react-router';// Import React Router
+import { useState, useEffect, useContext } from 'react';
 import NavBar from './components/NavBar/NavBar';
 // Import the SignUpForm component
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
-import { useContext } from 'react';
+import MachineDetails from './components/MachineDetails/MachineDetails';
+// import { useContext } from 'react';
 import { UserContext } from './contexts/UserContext';
+import './App.css';
 
 const App = () => {
   const { user } = useContext(UserContext);
@@ -24,10 +26,7 @@ const App = () => {
           <>
             <Route path='/' element={<Dashboard/>}/>
             <Route path="/machines/:id" element={<MachineDetails />} />
-            <Route path='/products' element={<h1>Producs</h1>}/>
-            <Route path='/favs' element={<h1>Favs</h1>}/>
             <Route path='/profile' element={<h1>{user.username}</h1>}/>
-            <Route path='/orders' element={<h1>ORDERS</h1>}/>
           </>
             :
             <Route path='/' element={<Landing/>}/>
